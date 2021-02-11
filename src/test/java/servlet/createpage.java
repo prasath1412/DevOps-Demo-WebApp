172f6706043f89ef7b5a7494078884e3cebc0c34
Prasath Pitchai Pillai <78113979+prasath1412@users.noreply.github.com>
2021-02-10 22:43:03 -0700

    Update createpage.java
    update IP

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://18.222.225.123:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://18.224.138.232:5432/postgres";
 			String username="postgres";
 			String password="password";
 		
